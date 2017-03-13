4466f34085093fa95c7db5f7db0b28deaaa67c4c
dan <dansnyd3r@gmail.com>
2017-03-13 01:10:41 -0400

    failbook

--- client/app/components/errorbanner.js
+++ client/app/components/errorbanner.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {hideElement} from '../util';
 
+/**
+ * A yellow error banner that uses Bootstrap's "warning" alert. Used to display HTTP request failures.
+ */
 export default class ErrorBanner extends React.Component {
   constructor(props) {
     super(props);
@@ -8,8 +11,9 @@ export default class ErrorBanner extends React.Component {
       active: false,
       errors: ""
     };
-    
+
     // ASSUMPTION: There is only one ErrorBanner component ever created.
+    // (Otherwise, each will overwrite one another's global function...)
     // By assigning to 'window', this is a global function. Global functions
     // are not typically a good idea, but they can be useful for adding basic
     // error handling to an application
@@ -20,8 +24,9 @@ export default class ErrorBanner extends React.Component {
       })
     };
   }
-  
+
   render() {
+    // Don't display the error banner unless 'this.state.active' is true.
     return (
       <div className={"alert alert-warning " + hideElement(!this.state.active)} role="alert">
         Facebook was unable to complete a recent request: {this.state.error}<br />
